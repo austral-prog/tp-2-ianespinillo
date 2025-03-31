27d373dd1d504e33f8a2843cb42c309da13534fd
Ian Espinillo <iespinillo@mail.austral.edu.ar>
2025-03-31 14:12:45 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,7 @@
 def change():
     expense = 23.75
     money = 100
+    vuelto = money - expense
+    print('Vuelto')
+    print (f"Pesos: {vuelto}")
+    print (f"Centavos: .{str(vuelto).split('.')[1]}")
